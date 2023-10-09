7f9a80c55ac118cd1ec2282dd403aceb8337b5b3
TinhMai123 <Tinhmvqse173245@fpt.edu.vn>
2023-10-09 08:54:11 +0700

    Nhanvien va chim

--- bird/components/Bird/Table.jsx
+++ bird/components/Bird/Table.jsx
@@ -9,17 +9,17 @@ const Table = () => {
         <th scope="col">Bird Type</th>
         <th scope="col">Sex</th>
         <th scope="col">Birthday</th>
-        <th scope="col">Weight</th>
+        <th scope="col">Weight (g)</th>
         
       </tr>
     </thead>
     <tbody>
       <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>@mdo</td>
+        <a href='/bird/id'><th scope="row">A01</th></a>
+        <td>Chích chòe Than</td>
+        <td>Trống</td>
+        <td>08/09/2023</td>
+        <td>300</td>
       </tr>
       
     </tbody>
